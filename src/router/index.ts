c72d48c5253501311c7b5045b549bbc147329c75
hs <770142345@qq.com>
2023-03-31 10:11:41 +0800

    first：提交所有文件

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  { 
+    path: '/', 
+    redirect: '/login' 
+  }, 
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/login/login.vue')
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: () => import('@/views/home/home.vue')
+  },
+  { 
+    name: 'NotFound', 
+    path: '/:pathMatch(.*)*', 
+    component: () => import('@/views/not-found/not-found.vue') 
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
